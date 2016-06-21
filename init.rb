83be1870193c1dfe4be05fb4d3fb73882e1bdc95
David Gómez <david.gomez@marsbased.com>
2016-06-21 18:11:21 +0200

    Updates Ubiquo Config to Settings.

--- init.rb
+++ init.rb
@@ -3,15 +3,15 @@ require 'translate'
 I18n::Backend::Simple.send(:include, I18n::Backend::I18nTranslateBackend)
 
 config.after_initialize do
-  Translate::Storage.mode = if defined?(Ubiquo::Config) && Ubiquo::Config.option_exists?(:translate_mode)
-    Ubiquo::Config.get(:translate_mode)
+  Translate::Storage.mode = if defined?(Ubiquo::Settings) && Ubiquo::Settings.option_exists?(:translate_mode)
+    Ubiquo::Settings.get(:translate_mode)
   else
     :application
   end
 
   def I18n.supported_locales
-    if defined?(Ubiquo::Config) && Ubiquo::Config.option_exists?(:supported_locales)
-      Ubiquo::Config.get(:supported_locales)
+    if defined?(Ubiquo::Settings) && Ubiquo::Settings.option_exists?(:supported_locales)
+      Ubiquo::Settings.get(:supported_locales)
     else
       I18n.available_locales
     end
